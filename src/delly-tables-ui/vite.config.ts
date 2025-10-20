9c095c77bf88ad9c03380fdaa6d5f09c2d7376c7
joshdellamuth <73405763+joshdellamuth@users.noreply.github.com>
2025-10-20 00:02:26 -0500

    Adding file resolution to vite config and ts paths to fix GitHub pages isssues

--- src/delly-tables-ui/vite.config.ts
+++ src/delly-tables-ui/vite.config.ts
@@ -1,9 +1,17 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import { resolve } from 'path';
+
 export default defineConfig({
     base: "/delly-tables/",
     build: {
         outDir: 'dist',
-        emptyOutDir: true
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                admin: resolve(__dirname, 'admin.html'),
+            }
+        }
     },
     server: {
         open: true
